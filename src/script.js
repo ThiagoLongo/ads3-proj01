c5d5acaafa5887155dcec840c48f3b70fd204feb
DESKTOP-VNBD8DN\User <thiagolongo012@gmail.com>
2025-02-26 20:06:14 -0300

    meu primeiro commit

---
+++ src/script.js
@@ -0,0 +1,11 @@
+function calcularIdade(){
+
+    let anoDigitado = 
+    document.getElementById("anoNascimento").value; 
+
+    let idade = 2025 - anoDigitado;
+
+    console.log(idade);
+
+    document.getElementById("resultado").innerHTML = `idade ${idade}`;
+}
